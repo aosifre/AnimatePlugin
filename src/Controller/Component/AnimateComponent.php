a3ec0ece6ff0d07cf00cb441e43e7d314ec2b0d2
Osifre Alain <osifre.alain@gmail.com>
2016-12-18 12:09:59 +0100

    Animate component

---
+++ src/Controller/Component/AnimateComponent.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace AnimatePlugin\Controller\Component;
+
+use Cake\Controller\Component;
+
+/**
+ * Class AnimateComponent
+ *
+ * @package Animate\Controller\Component
+ */
+class AnimateComponent extends Component
+{
+    const CLASS_BOUNCE = 'bounce';
+
+    const CLASS_FLASH = 'flash';
+
+    const CLASS_PULSE = 'pulse';
+
+    const CLASS_RUBBERBAND = 'rubberBand';
+
+    const CLASS_SHAKE = 'shake';
+    const CLASS_HEAD_SHAKE = 'headShake';
+
+    const CLASS_SWING = 'swing';
+
+    const CLASS_TADA = 'tada';
+
+    const CLASS_WOBBLE = 'wobble';
+
+    const CLASS_JELLO = 'jello';
+
+    const CLASS_BOUNCE_IN = 'bounceIn';
+    const CLASS_BOUNCE_IN_DOWN = 'bounceInDown';
+    const CLASS_BOUNCE_IN_LEFT = 'bounceInLeft';
+    const CLASS_BOUNCE_IN_RIGHT = 'bounceInRight';
+    const CLASS_BOUNCE_IN_UP = 'bounceInUp';
+    const CLASS_BOUNCE_OUT = 'bounceOut';
+    const CLASS_BOUNCE_OUT_DOWN = 'bounceOutDown';
+    const CLASS_BOUNCE_OUT_LEFT = 'bounceOutLeft';
+    const CLASS_BOUNCE_OUT_RIGHT = 'bounceOutRight';
+    const CLASS_BOUNCE_OUT_UP = 'bounceOutUp';
+
+    const CLASS_FADE_IN = 'fadeIn';
+    const CLASS_FADE_IN_DOWN = 'fadeInDown';
+    const CLASS_FADE_IN_DOWN_BIG = 'fadeInDownBig';
+    const CLASS_FADE_IN_LEFT = 'fadeInLeft';
+    const CLASS_FADE_IN_LEFT_BIG = 'fadeInLeftBig';
+    const CLASS_FADE_IN_RIGHT = 'fadeInRight';
+    const CLASS_FADE_IN_RIGHT_BIG = 'fadeInRightBig';
+    const CLASS_FADE_IN_UP = 'fadeInUp';
+    const CLASS_FADE_IN_UP_BIG = 'fadeInUpBig';
+    const CLASS_FADE_OUT = 'fadeOut';
+    const CLASS_FADE_OUT_DOWN = 'fadeOutDown';
+    const CLASS_FADE_OUT_DOWN_BIG = 'fadeOutDownBig';
+    const CLASS_FADE_OUT_LEFT = 'fadeOutLeft';
+    const CLASS_FADE_OUT_LEFT_BIG = 'fadeOutLeftBig';
+    const CLASS_FADE_OUT_RIGHT = 'fadeOutRight';
+    const CLASS_FADE_OUT_RIGHT_BIG = 'fadeOutRightBig';
+    const CLASS_FADE_OUT_UP = 'fadeOutUp';
+    const CLASS_FADE_OUT_UP_BIG = 'fadeOutUpBig';
+
+    const CLASS_FLIP_IN_X = 'flipInX';
+    const CLASS_FLIP_IN_Y = 'flipInY';
+    const CLASS_FLIP_OUT_X = 'flipOutX';
+    const CLASS_FLIP_OUT_Y = 'flipOutY';
+
+    const CLASS_LIGHT_SPEED_IN = 'lightSpeedIn';
+    const CLASS_LIGHT_SPEED_OUT = 'lightSpeedOut';
+
+    const CLASS_ROTATE_IN = 'rotateIn';
+    const CLASS_ROTATE_IN_DOWN_LEFT = 'rotateInDownLeft';
+    const CLASS_ROTATE_IN_DOWN_RIGHT = 'rotateInDownRight';
+    const CLASS_ROTATE_IN_UP_LEFT = 'rotateInUpLeft';
+    const CLASS_ROTATE_IN_UP_RIGHT = 'rotateInUpRight';
+    const CLASS_ROTATE_OUT = 'rotateOut';
+    const CLASS_ROTATE_OUT_DOWN_LEFT = 'rotateOutDownLeft';
+    const CLASS_ROTATE_OUT_DOWN_RIGHT = 'rotateOutDownRight';
+    const CLASS_ROTATE_OUT_UP_LEFT = 'rotateOutUpLeft';
+    const CLASS_ROTATE_OUT_UP_RIGHT = 'rotateOutUpRight';
+
+    const CLASS_HINGE = 'hinge';
+
+    const CLASS_ROOL_IN = 'rollIn';
+    const CLASS_ROLL_OUT = 'rollOut';
+
+    const CLASS_ZOOM_IN = 'zoomIn';
+    const CLASS_ZOOM_IN_DOWN = 'zoomInDown';
+    const CLASS_ZOOM_IN_LEFT = 'zoomInLeft';
+    const CLASS_ZOOM_IN_RIGHT = 'zoomInRight';
+    const CLASS_ZOOM_IN_UP = 'zoomInUp';
+    const CLASS_ZOOM_OUT = 'zoomOut';
+    const CLASS_ZOOM_OUT_DOWN = 'zoomOutDown';
+    const CLASS_ZOOM_OUT_LEFT = 'zoomOutLeft';
+    const CLASS_ZOOM_OUT_RIGHT = 'zoomOutRight';
+    const CLASS_ZOOM_OUT_UP = 'zoomOutUp';
+
+    const CLASS_SLIDE_DOWN = 'slideInDown';
+    const CLASS_SLIDE_IN_LEFT = 'slideInLeft';
+    const CLASS_SLIDE_IN_RIGHT = 'slideInRight';
+    const CLASS_SLIDE_IN_UP = 'slideInUp';
+    const CLASS_SLIDE_OUT_DOWN = 'slideOutDown';
+    const CLASS_SLIDE_OUT_LEFT = 'slideOutLeft';
+    const CLASS_SLIDE_OUT_RIGHT = 'slideOutRight';
+    const CLASS_SLIDE_OUT_UP = 'slideOutUp';
+}
